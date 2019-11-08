42929a2d7620917be55f8fc0ea4d896e9cd158cb
thanadolps <thanadolps@gmail.com>
2019-11-08 19:50:39 +0700

    Add support for software serial

--- arduino-board/slave.cpp
+++ arduino-board/slave.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 #include "order.h"
 #include "slave.h"
@@ -212,4 +213,78 @@ void write_i32(int32_t num)
 {
 	int8_t buffer[4] = {(int8_t) (num & 0xff), (int8_t) (num >> 8 & 0xff), (int8_t) (num >> 16 & 0xff), (int8_t) (num >> 24 & 0xff)};
   Serial.write((uint8_t*)&buffer, 4*sizeof(int8_t));
+}
+
+
+
+Order read_order(SoftwareSerial& serial)
+{
+	return (Order) serial.read();
+}
+
+void wait_for_bytes(SoftwareSerial& serial, int num_bytes, unsigned long timeout)
+{
+	unsigned long startTime = millis();
+	//Wait for incoming bytes or exit if timeout
+	while ((serial.available() < num_bytes) && (millis() - startTime < timeout)){}
+}
+
+// NOTE : Serial.readBytes is SLOW
+// this one is much faster, but has no timeout
+void read_signed_bytes(SoftwareSerial& serial, int8_t* buffer, size_t n)
+{
+	size_t i = 0;
+	int c;
+	while (i < n)
+	{
+		c = serial.read();
+		if (c < 0) break;
+		*buffer++ = (int8_t) c; // buffer[i] = (int8_t)c;
+		i++;
+	}
+}
+
+int8_t read_i8(SoftwareSerial& serial)
+{
+	wait_for_bytes(1, 100); // Wait for 1 byte with a timeout of 100 ms
+  return (int8_t) serial.read();
+}
+
+int16_t read_i16(SoftwareSerial& serial)
+{
+  int8_t buffer[2];
+	wait_for_bytes(2, 100); // Wait for 2 bytes with a timeout of 100 ms
+	read_signed_bytes(buffer, 2);
+  return (((int16_t) buffer[0]) & 0xff) | (((int16_t) buffer[1]) << 8 & 0xff00);
+}
+
+int32_t read_i32(SoftwareSerial& serial)
+{
+  int8_t buffer[4];
+	wait_for_bytes(4, 200); // Wait for 4 bytes with a timeout of 200 ms
+	read_signed_bytes(buffer, 4);
+  return (((int32_t) buffer[0]) & 0xff) | (((int32_t) buffer[1]) << 8 & 0xff00) | (((int32_t) buffer[2]) << 16 & 0xff0000) | (((int32_t) buffer[3]) << 24 & 0xff000000);
+}
+
+void write_order(SoftwareSerial& serial, enum Order myOrder)
+{
+	uint8_t* Order = (uint8_t*) &myOrder;
+  serial.write(Order, sizeof(uint8_t));
+}
+
+void write_i8(SoftwareSerial& serial, int8_t num)
+{
+  serial.write(num);
+}
+
+void write_i16(SoftwareSerial& serial, int16_t num)
+{
+	int8_t buffer[2] = {(int8_t) (num & 0xff), (int8_t) (num >> 8)};
+  serial.write((uint8_t*)&buffer, 2*sizeof(int8_t));
+}
+
+void write_i32(SoftwareSerial& serial, int32_t num)
+{
+	int8_t buffer[4] = {(int8_t) (num & 0xff), (int8_t) (num >> 8 & 0xff), (int8_t) (num >> 16 & 0xff), (int8_t) (num >> 24 & 0xff)};
+  serial.write((uint8_t*)&buffer, 4*sizeof(int8_t));
 }
--- arduino-board/slave.h
+++ arduino-board/slave.h
@@ -88,4 +88,70 @@ void write_i32(int32_t num);
  */
 void get_messages_from_serial();
 
+
+
+/*!
+ * \brief Read one byte from the serial and cast it to an Order
+ * \return the order received
+ */
+Order read_order(SoftwareSerial& serial);
+
+/*!
+ * \brief Wait until there are enough bytes in the buffer
+ * \param num_bytes the number of bytes
+ * \param timeout (ms) The timeout, time after which we release the lock
+ * even if there are not enough bytes
+ */
+void wait_for_bytes(SoftwareSerial& serial, int num_bytes, unsigned long timeout);
+
+/*!
+ * \brief Read signed bytes and put them in a buffer
+ * \param buffer an array of bytes
+ * \param n number of bytes to read
+ */
+void read_signed_bytes(SoftwareSerial& serial, int8_t* buffer, size_t n);
+
+/*!
+ * \brief Read one byte from a serial port and convert it to a 8 bits int
+ * \return the decoded number
+ */
+int8_t read_i8(SoftwareSerial& serial);
+
+/*!
+ * \brief Read two bytes from a serial port and convert it to a 16 bits int
+ * \return the decoded number
+ */
+int16_t read_i16(SoftwareSerial& serial);
+
+
+/*!
+ * \brief Read four bytes from a serial port and convert it to a 32 bits int
+ * \return the decoded number
+ */
+int32_t read_i32(SoftwareSerial& serial);
+
+/*!
+ * \brief Send one order (one byte)
+ * \param order type of order
+ */
+void write_order(SoftwareSerial& serial, enum Order order);
+
+/*!
+ * \brief Write one byte int to serial port (between -127 and 127)
+ * \param num an int of one byte
+ */
+void write_i8(SoftwareSerial& serial, int8_t num);
+
+/*!
+ * \brief Send a two bytes signed int via the serial
+ * \param num the number to send (max: (2**16/2 -1) = 32767)
+ */
+void write_i16(SoftwareSerial& serial, int16_t num);
+
+/*!
+ * \brief Send a four bytes signed int (long) via the serial
+ * \param num the number to send (−2,147,483,647, +2,147,483,647)
+ */
+void write_i32(SoftwareSerial& serial, int32_t num);
+
 #endif
